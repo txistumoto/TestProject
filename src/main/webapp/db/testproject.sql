9ef23f55cdb2e249ce87ad74fc3dc5cdd97b5e64
pgonzalez <pgonzalez@sqs.es>
2015-08-27 14:12:34 +0200

    Database scripts correction

--- src/main/webapp/db/testproject.sql
+++ src/main/webapp/db/testproject.sql
@@ -1,57 +1,80 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         5.6.16 - MySQL Community Server (GPL)
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.1.0.4867
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.4.13.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 27-08-2015 a las 12:02:18
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 
--- Volcando estructura de base de datos para testproject
--- DROP DATABASE IF EXISTS `testproject`;
--- CREATE DATABASE IF NOT EXISTS `testproject` 
+--
+-- Base de datos: `testproject`
+--
+
 CREATE DATABASE IF NOT EXISTS `testproject` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `testproject`;
 
--- Volcando estructura para tabla testproject.apks
--- DROP TABLE IF EXISTS `apks`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `apks`
+--
+
 CREATE TABLE IF NOT EXISTS `apks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `file` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `pack` varchar(50) NOT NULL,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_date` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `update_date` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- Volcando estructura para disparador testproject.apks_BINS
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `apks_BINS` BEFORE INSERT ON `apks` FOR EACH ROW BEGIN
+--
+-- Disparadores `apks`
+--
+DELIMITER $$
+CREATE TRIGGER `apks_BINS` BEFORE INSERT ON `apks`
+ FOR EACH ROW BEGIN
 SET NEW.update_date =  NOW();
-END//
+END
+$$
 DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
-
-
--- Volcando estructura para disparador testproject.apks_BUPD
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `apks_BUPD` BEFORE UPDATE ON `apks` FOR EACH ROW BEGIN
+DELIMITER $$
+CREATE TRIGGER `apks_BUPD` BEFORE UPDATE ON `apks`
+ FOR EACH ROW BEGIN
 SET NEW.update_date =  NOW();
-END//
+END
+$$
 DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `apks`
+--
+ALTER TABLE `apks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `apks`
+--
+ALTER TABLE `apks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
--- Volcando datos para la tabla testproject.apks: ~6 rows (aproximadamente)
-/*!40000 ALTER TABLE `apks` DISABLE KEYS */;
-INSERT INTO `apks` (`id`, `file`, `name`, `pack`, `creation_date`, `update_date`) VALUES
-	(1, 'PreReqAgile.apk', 'sqs.prereqagile', 'PreReqAgileActivity', current_timestamp, current_timestamp);
-/*!40000 ALTER TABLE `apks` ENABLE KEYS */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
